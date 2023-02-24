7838aef7980cdd34ff692dbb6b9e3b7bbd950fbc
aashishmuttreja <50440645+aashishmuttreja@users.noreply.github.com>
2023-02-24 16:36:31 +0530

    Add files via upload

---
+++ webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BDFqRKB4j8kyg-m2nlCN4wKgDMQ7yvsd_erYDxgHUVYCOF66Eu9lg0ALw-k17GxwAaoaoLGq_RbjarOjTlHgwTI=","apiKey":"ABEvV5bLGNt3Ej8fyEZJJo5h4","accountToken":"ABEq_61sebCfTlmtGZ-mOxe88","appver":"0.0.0","apiHost":"https://api.pushio.com","lazy":false}';
+orawp.init(config);
